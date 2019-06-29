cab77676853c625c40d94488f31f8493d5c3fccc
Lucas Caton <lucascaton@gmail.com>
2019-06-29 10:07:50 +1000

    Create Programming languages endpoints specs

---
+++ spec/requests/api/v1/programming_languages_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe 'Programming languages endpoints', type: :request do
+  context 'when there are no programming languages' do
+    it 'returns an empty list' do
+      get '/api/v1/programming_languages'
+
+      expect(JSON.parse(response.body)).to be_empty
+    end
+  end
+
+  context 'when there are programming languages' do
+    before do
+      ProgrammingLanguage.create name: 'Ruby', release_year: '1995', creator: 'Matz'
+      ProgrammingLanguage.create name: 'Lisp', release_year: '1958', creator: 'John McCarthy'
+    end
+
+    it 'returns 2 items' do
+      get '/api/v1/programming_languages'
+
+      expect(JSON.parse(response.body).count).to eq(2)
+    end
+
+    it 'returns a list' do
+      get '/api/v1/programming_languages'
+
+      expect(JSON.parse(response.body).map(&:symbolize_keys).first).to include(
+        name: 'Lisp',
+        release_year: 1958,
+        creator: 'John McCarthy'
+      )
+
+      expect(JSON.parse(response.body).map(&:symbolize_keys).last).to include(
+        name: 'Ruby',
+        release_year: 1995,
+        creator: 'Matz'
+      )
+    end
+  end
+end
