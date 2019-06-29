fc168e49f2caeb670bf43a17234d2bfd9dfec663
Lucas Caton <lucascaton@gmail.com>
2019-06-29 10:43:35 +1000

    Create another context for programming languages endpoints specs

--- spec/requests/api/v1/programming_languages_spec.rb
+++ spec/requests/api/v1/programming_languages_spec.rb
@@ -1,50 +1,52 @@
 RSpec.describe 'Programming languages endpoints', type: :request do
-  context 'when there are no programming languages' do
-    it 'returns an empty list' do
-      get '/api/v1/programming_languages'
+  context 'GET index' do
+    context 'when there are no programming languages' do
+      it 'returns an empty list' do
+        get '/api/v1/programming_languages'
 
-      expect(JSON.parse(response.body)).to be_empty
-    end
-  end
-
-  context 'when there are programming languages' do
-    before do
-      create(:programming_language, name: 'Ruby', release_year: '1995', creator: 'Matz')
-      create(:programming_language, name: 'Lisp', release_year: '1958', creator: 'John McCarthy')
+        expect(JSON.parse(response.body)).to be_empty
+      end
     end
 
-    it 'returns 2 items' do
-      get '/api/v1/programming_languages'
+    context 'when there are programming languages' do
+      before do
+        create(:programming_language, name: 'Ruby', release_year: '1995', creator: 'Matz')
+        create(:programming_language, name: 'Lisp', release_year: '1958', creator: 'John McCarthy')
+      end
 
-      expect(JSON.parse(response.body).count).to eq(2)
-    end
+      it 'returns 2 items' do
+        get '/api/v1/programming_languages'
 
-    it 'returns a list' do
-      get '/api/v1/programming_languages'
+        expect(JSON.parse(response.body).count).to eq(2)
+      end
 
-      expect(JSON.parse(response.body).map(&:symbolize_keys).first).to include(
-        name: 'Lisp',
-        release_year: 1958,
-        creator: 'John McCarthy'
-      )
+      it 'returns a list' do
+        get '/api/v1/programming_languages'
 
-      expect(JSON.parse(response.body).map(&:symbolize_keys).last).to include(
-        name: 'Ruby',
-        release_year: 1995,
-        creator: 'Matz'
-      )
-    end
+        expect(JSON.parse(response.body).map(&:symbolize_keys).first).to include(
+          name: 'Lisp',
+          release_year: 1958,
+          creator: 'John McCarthy'
+        )
 
-    it 'returns the correct HTTP status' do
-      get '/api/v1/programming_languages'
+        expect(JSON.parse(response.body).map(&:symbolize_keys).last).to include(
+          name: 'Ruby',
+          release_year: 1995,
+          creator: 'Matz'
+        )
+      end
 
-      expect(response).to have_http_status(:ok)
-    end
+      it 'returns the correct HTTP status' do
+        get '/api/v1/programming_languages'
 
-    it 'returns the correct content_type' do
-      get '/api/v1/programming_languages'
+        expect(response).to have_http_status(:ok)
+      end
 
-      expect(response.content_type).to eq('application/json')
+      it 'returns the correct content_type' do
+        get '/api/v1/programming_languages'
+
+        expect(response.content_type).to eq('application/json')
+      end
     end
   end
 end
